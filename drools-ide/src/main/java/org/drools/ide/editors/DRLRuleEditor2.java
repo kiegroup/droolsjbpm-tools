74963f96f5a87271cc91fe2a768421398f753d71
Kris Verlaenen <kris.verlaenen@gmail.com>
2006-03-20 00:52:01 +0000

    first version of a Reteoo viewer in ide
    git-svn-id: https://svn.jboss.org/repos/labs/trunk/labs/jbossrules@3046 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-ide/src/main/java/org/drools/ide/editors/DRLRuleEditor2.java
@@ -0,0 +1,54 @@
+package org.drools.ide.editors;
+
+import org.drools.ide.DroolsIDEPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.forms.editor.FormEditor;
+
+/**
+ * Generic rule editor for drools.
+ * @author Michael Neale
+ */
+public class DRLRuleEditor2 extends FormEditor {
+
+	private DRLRuleEditor textEditor;
+
+	private ReteViewer reteViewer;
+
+	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+		super.init(site, input);
+		setPartName(input.getName());
+	}
+
+	protected void addPages() {
+		try {
+			textEditor = new DRLRuleEditor();
+			reteViewer = new ReteViewer(this, textEditor.getDocumentProvider());
+
+			int text = addPage(textEditor, getEditorInput());
+			int rete = addPage(reteViewer, getEditorInput());
+
+			setPageText(text, "Text Editor");
+			setPageText(rete, "Rete Tree");
+		} catch (PartInitException e) {
+			DroolsIDEPlugin.log(e);
+		}
+	}
+
+	public void doSave(IProgressMonitor monitor) {
+		textEditor.doSave(monitor);
+		setInput(getEditorInput());
+		reteViewer.clear();
+	}
+
+	public void doSaveAs() {
+		textEditor.doSaveAs();
+	}
+
+	public boolean isSaveAsAllowed() {
+		return textEditor.isSaveAsAllowed();
+	}
+
+}
---
+++ drools-ide/src/main/java/org/drools/ide/editors/ReteViewer.java
@@ -0,0 +1,143 @@
+package org.drools.ide.editors;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+
+import org.drools.RuleBase;
+import org.drools.compiler.DrlParser;
+import org.drools.compiler.PackageBuilder;
+import org.drools.ide.DroolsIDEPlugin;
+import org.drools.lang.descr.PackageDescr;
+import org.drools.reteoo.RuleBaseImpl;
+import org.drools.rule.Package;
+import org.drools.visualize.ReteooJungViewerPanel;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+
+public class ReteViewer extends EditorPart {
+
+	private DRLRuleEditor2 drlEditor;
+
+	private IDocumentProvider documentProvider;
+
+	private Frame frame;
+
+	private Composite parent;
+
+	public ReteViewer(DRLRuleEditor2 drlEditor,
+			IDocumentProvider documentProvider) {
+		this.drlEditor = drlEditor;
+		this.documentProvider = documentProvider;
+	}
+
+	public void createPartControl(Composite parent) {
+		this.parent = parent;
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 1;
+		parent.setLayout(layout);
+		Button generateButton = new Button(parent, SWT.PUSH);
+		generateButton.setText("Generate Rete View");
+		generateButton.addSelectionListener(new SelectionListener() {
+			public void widgetSelected(SelectionEvent e) {
+				generateReteView();
+			}
+
+			public void widgetDefaultSelected(SelectionEvent e) {
+				generateReteView();
+			}
+		});
+
+		Composite frameParent = new Composite(parent, SWT.EMBEDDED);
+		frameParent.setLayoutData(new GridData(GridData.FILL_BOTH));
+		frame = SWT_AWT.new_Frame(frameParent);
+		frame.setLayout(new BorderLayout());
+	}
+
+	private RuleBase getRuleBase() {
+		if (getEditorInput() instanceof IFileEditorInput) {
+			try {
+				System.out.println(documentProvider);
+				String contents = documentProvider.getDocument(getEditorInput()).get();
+				DrlParser parser = new DrlParser();
+				PackageDescr packageDescr = parser.parse(contents);
+
+				//pre build the package
+				PackageBuilder builder = new PackageBuilder();
+				builder.addPackage(packageDescr);
+				Package pkg = builder.getPackage();
+
+				//add the package to a rulebase
+				RuleBaseImpl ruleBase = new RuleBaseImpl();
+				ruleBase.addPackage(pkg);
+				return ruleBase;
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	public void doSave(IProgressMonitor monitor) {
+		// Do nothing
+	}
+
+	public void doSaveAs() {
+		// Do nothing
+	}
+
+	public void init(IEditorSite site, IEditorInput input)
+			throws PartInitException {
+		setSite(site);
+		setInput(input);
+	}
+
+	public void clear() {
+		frame.removeAll();
+	}
+
+	public boolean isDirty() {
+		return false;
+	}
+
+	public boolean isSaveAsAllowed() {
+		return false;
+	}
+
+	public void setFocus() {
+		if (drlEditor.isDirty()) {
+			clear();
+		}
+	}
+
+	private void generateReteView() {
+		clear();
+		try {
+			RuleBase ruleBase = getRuleBase();
+			if (ruleBase == null) {
+				// TODO signal user that rule cannot be parsed
+			} else {
+				ReteooJungViewerPanel viewer = new ReteooJungViewerPanel(
+						ruleBase);
+				frame.add(viewer);
+				frame.validate();
+				parent.layout();
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+			DroolsIDEPlugin.log(t);
+		}
+	}
+}
