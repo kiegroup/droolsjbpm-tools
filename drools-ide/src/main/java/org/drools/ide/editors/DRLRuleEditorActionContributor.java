894e7796552cef2eff673fc8bcaf576f278e4fb7
Kris Verlaenen <kris.verlaenen@gmail.com>
2006-05-02 15:59:52 +0000

    made DRLRuleEditor2 propagate actions to DRLRuleEditor
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@4042 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ drools-ide/src/main/java/org/drools/ide/editors/DRLRuleEditorActionContributor.java
@@ -0,0 +1,29 @@
+package org.drools.ide.editors;
+
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.editors.text.TextEditorActionContributor;
+import org.eclipse.ui.part.MultiPageEditorActionBarContributor;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+public class DRLRuleEditorActionContributor extends MultiPageEditorActionBarContributor {
+
+	private TextEditorActionContributor contributor = new TextEditorActionContributor();
+	
+	public void init(IActionBars bars, IWorkbenchPage page) {
+		contributor.init(bars);
+		super.init(bars, page);
+	}
+		
+	public void setActivePage(IEditorPart activeEditor) {
+		IActionBars bars = getActionBars();
+		System.out.println(bars);
+		if (activeEditor instanceof ITextEditor) {
+			if (bars != null) {
+				contributor.setActiveEditor(activeEditor);
+			}
+		}
+	}
+
+}
