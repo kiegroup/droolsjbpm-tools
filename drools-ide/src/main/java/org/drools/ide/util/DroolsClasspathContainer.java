077f4fa03007bdaaf6e3370ba34e01fec753f633
Michael Neale <michael.neale@gmail.com>
2006-06-27 08:51:41 +0000

    changed to 1.4
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@4836 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-ide/src/main/java/org/drools/ide/util/DroolsClasspathContainer.java
+++ drools-ide/src/main/java/org/drools/ide/util/DroolsClasspathContainer.java
@@ -61,7 +61,7 @@ public class DroolsClasspathContainer implements IClasspathContainer {
         File pluginRoot = new Path(pluginRootString).toFile();
         File[] files = pluginRoot.listFiles();
         for (int i = 0; i < files.length; i++) {
-	        if (files[i].getAbsolutePath().contains("org.eclipse.jdt.core_")) {
+	        if (files[i].getAbsolutePath().indexOf("org.eclipse.jdt.core_") > -1) {
 	        	Path path = new Path(files[i].getAbsolutePath());
 	        	list.add(JavaCore.newLibraryEntry(path, path, null));
 	        }
