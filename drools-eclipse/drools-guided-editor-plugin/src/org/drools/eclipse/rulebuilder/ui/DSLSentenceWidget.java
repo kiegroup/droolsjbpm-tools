65a539ee59247de3ec83617e7d2a0068ddb59c04
Anton Arhipov <ant.arhipov@gmail.com>
2007-06-04 20:54:09 +0000

    layout complete
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@12323 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-eclipse/drools-guided-editor-plugin/src/org/drools/eclipse/rulebuilder/ui/DSLSentenceWidget.java
+++ drools-eclipse/drools-guided-editor-plugin/src/org/drools/eclipse/rulebuilder/ui/DSLSentenceWidget.java
@@ -1,12 +1,19 @@
 package org.drools.eclipse.rulebuilder.ui;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.drools.brms.client.modeldriven.brxml.DSLSentence;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+
 
 /**
  * This displays a widget to edit a DSL sentence.
@@ -18,57 +25,75 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 public class DSLSentenceWidget extends Widget {
 
     private final DSLSentence sentence;
-
+    
+    private List widgets = new ArrayList();
+    
     public DSLSentenceWidget(FormToolkit toolkit,
                              Composite parent,
                              DSLSentence sentence, RuleModeller modeller, int index) {
         super(parent,toolkit,modeller,index);
         
         this.sentence = sentence;
-        init();
-    }
-
-    private void init() {
+        
         GridLayout l = new GridLayout();
-        l.numColumns = sentence.sentence.length();//sentence.elements.length;
+        l.numColumns = sentence.sentence.length();
         l.verticalSpacing = 0;
         l.marginTop = 0;
         l.marginHeight = 2;
         l.marginBottom = 0;
         parent.setLayout( l );
-
-        toolkit.createLabel( parent,
-                             sentence.toString() );
-
-        final Text box = toolkit.createText( parent,
-                                             sentence.toString() );
-        box.setTabs( sentence.toString().length() );
-
-        box.addModifyListener( new ModifyListener() {
-            public void modifyText(ModifyEvent e) {
-                sentence.sentence = box.getText();
-                getModeller().setDirty( true );
-            }
-        } );
-
-        /*for (int i = 0; i < sentence.length; i++) {
-         //final DSLSentenceFragment el = sentence.elements[i];
-         
-         if (!el.isEditableField) {
-         toolkit.createLabel(parent, sentence.toString());
-         } else {
-         final Text box = toolkit.createText(parent, el.value);
-         box.setTabs(el.value.length());
-
-         box.addModifyListener(new ModifyListener() {
-         public void modifyText(ModifyEvent e) {
-         el.value = box.getText();
-         }
-         });
-
-         }
-         }*/
-        toolkit.paintBordersFor( parent );
+        
+        makeWidget(sentence.sentence);
     }
+
+    private void makeWidget(String dslLine) {
+    	char[] chars = dslLine.toCharArray();
+        Text currentBox = null;
+        Label currentLabel = null;
+        for ( int i = 0; i < chars.length; i++ ) {
+            char c = chars[i];
+            if (c == '{') {
+                currentLabel = null;
+                currentBox = toolkit.createText(parent, ""); 
+                
+                currentBox.addModifyListener(new ModifyListener(){
+					public void modifyText(ModifyEvent e) {
+						updateSentence();
+					}
+                });
+                
+                widgets.add(currentBox);
+                
+            } else if (c == '}') {
+            	currentBox = null;
+            } else {
+                if (currentBox == null && currentLabel == null) {
+                    currentLabel = toolkit.createLabel(parent, "");
+                    widgets.add(currentLabel);
+                }
+                if (currentLabel != null) {
+                    currentLabel.setText( currentLabel.getText() + c );
+                } else if (currentBox != null) {
+                    currentBox.setText( currentBox.getText() + c );
+                }
+            }
+        }
+        
+        toolkit.paintBordersFor( parent );
+	}
+    
+    protected void updateSentence() {
+        String newSentence = "";
+        for ( Iterator iter = widgets.iterator(); iter.hasNext(); ) {
+            Control wid = (Control) iter.next();
+            if (wid instanceof Label) {
+                newSentence = newSentence + ((Label) wid).getText();
+            } else if (wid instanceof Text) {
+                newSentence = newSentence + " {" + ((Text) wid).getText() + "} ";
+            }
+        }
+        this.sentence.sentence = newSentence.trim();
+    }    
+    
 
 }
