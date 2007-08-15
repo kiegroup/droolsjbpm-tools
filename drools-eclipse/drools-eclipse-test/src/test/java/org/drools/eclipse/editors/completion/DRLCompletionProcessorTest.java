ccb5fda9f35d844dfdccc928d86f262f3111c10b
Philippe Ombredanne <pombredanne@nexb.com>
2007-08-15 06:37:00 +0000

    JBRULES-1084: fixed failing test, and added new ones for case ignorance
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@14239 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-eclipse/drools-eclipse-test/src/test/java/org/drools/eclipse/editors/completion/DRLCompletionProcessorTest.java
+++ drools-eclipse/drools-eclipse-test/src/test/java/org/drools/eclipse/editors/completion/DRLCompletionProcessorTest.java
@@ -10,24 +10,28 @@ public class DRLCompletionProcessorTest extends TestCase {
     public void testLookBehind() {
         assertEquals("something", CompletionUtil.stripLastWord(" something"));
         assertEquals("another", CompletionUtil.stripLastWord("another"));
-        
+
         String s = "rule something \n\nwhen";
         assertEquals("when", CompletionUtil.stripLastWord(s));
     }
-    
-    public void testPrefixFiltering() {
-        List list = new ArrayList();
-        list.add(new RuleCompletionProposal(0, 0, "aardvark", "something"));
-        list.add(new RuleCompletionProposal(0, 0, "smeg"));
-        list.add(new RuleCompletionProposal(0, 0, "apple"));
-        list.add(new RuleCompletionProposal(0, 0, "ape", "ape"));
-        
-        DefaultCompletionProcessor.filterProposalsOnPrefix("a", list);
-        assertEquals(2, list.size());
-        assertEquals("apple", list.get(0).toString());
-        assertEquals("ape", list.get(1).toString());
 
-        
+    public void testPrefixFiltering_FiltersBasedOnDisplayedStringNotContent() {
+        List list = new ArrayList();
+        list.add(new RuleCompletionProposal(0, 0, "abcd", "zxyz"));
+        list.add(new RuleCompletionProposal(0, 0, "azard","good"));
+        list.add(new RuleCompletionProposal(0, 0, "art","apple"));
+        list.add(new RuleCompletionProposal(0, 0, "spe", "ape"));
+
+        DefaultCompletionProcessor.filterProposalsOnPrefix("a", list);
+        assertEquals(3, list.size());
+        assertEquals("zxyz", list.get(0).toString());
+        assertEquals("good", list.get(1).toString());
+        assertEquals("apple", list.get(2).toString());
+
+    }
+
+    public void testPrefixFiltering_FiltersAllWhenThereisNoMatches() {
+        List list = new ArrayList();
         list = new ArrayList();
         list.add(new RuleCompletionProposal(0, 0, "aardvark", "something"));
         list.add(new RuleCompletionProposal(0, 0, "smeg"));
@@ -35,6 +39,22 @@ public class DRLCompletionProcessorTest extends TestCase {
         list.add(new RuleCompletionProposal(0, 0, "ape", "zzzzz"));
         DefaultCompletionProcessor.filterProposalsOnPrefix("xzyz", list);
         assertEquals(0, list.size());
+
     }
-    
+
+    public void testPrefixFiltering_IgnoreCase() {
+        List list = new ArrayList();
+        list = new ArrayList();
+        list.add(new RuleCompletionProposal(0, 0, "ART"));
+        list.add(new RuleCompletionProposal(0, 0, "art"));
+        list.add(new RuleCompletionProposal(0, 0, "aRT"));
+        list.add(new RuleCompletionProposal(0, 0, "Art", "zzzzz"));
+        DefaultCompletionProcessor.filterProposalsOnPrefix("art", list);
+        assertEquals(4, list.size());
+
+        DefaultCompletionProcessor.filterProposalsOnPrefix("ART", list);
+        assertEquals(4, list.size());
+
+    }
+
 }
