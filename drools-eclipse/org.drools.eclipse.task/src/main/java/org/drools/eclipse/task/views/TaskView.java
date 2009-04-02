4e710afede9481025f738ed3abaa7e3d04928cee
Dave Sinclair <stampy_88@yahoo.com>
2009-04-02 19:13:41 +0000

    TaskClientHandler constructor changed
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@25925 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-eclipse/org.drools.eclipse.task/src/main/java/org/drools/eclipse/task/views/TaskView.java
+++ drools-eclipse/org.drools.eclipse.task/src/main/java/org/drools/eclipse/task/views/TaskView.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.drools.SystemEventListenerFactory;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 import org.drools.eclipse.task.Activator;
 import org.drools.eclipse.task.preferences.DroolsTaskConstants;
@@ -56,21 +57,21 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 public class TaskView extends ViewPart {
-	
+
 	private static final String NAME_COLUMN = "Name";
 	private static final String STATUS_COLUMN = "Status";
 	private static final String OWNER_COLUMN = "Owner";
 	private static final String CREATED_COLUMN = "Created";
 	private static final String COMMENT_COLUMN = "Comment";
 
-	private static String[] columnNames = new String[] { 
-		NAME_COLUMN, 
+	private static String[] columnNames = new String[] {
+		NAME_COLUMN,
 		STATUS_COLUMN,
 		OWNER_COLUMN,
 		CREATED_COLUMN,
 		COMMENT_COLUMN
 	};
-	
+
 	private static Map<Status, String> STATUSSES;
 	static {
 		STATUSSES = new HashMap<Status, String>();
@@ -85,11 +86,11 @@ public class TaskView extends ViewPart {
 		STATUSSES.put(Status.Reserved, "Reserved");
 		STATUSSES.put(Status.Suspended, "Suspended");
 	}
-	
+
 	private String ipAddress = "127.0.0.1";
 	private int port = 9123;
 	private String language = "en-UK";
-	
+
 	private Text userNameText;
 	private Table table;
 	private TableViewer tableViewer;
@@ -103,7 +104,7 @@ public class TaskView extends ViewPart {
 	private Button skipButton;
 	private Button completeButton;
 	private Button failButton;
-	
+
 	private MinaTaskClient client;
 
 	private class ViewContentProvider implements IStructuredContentProvider {
@@ -120,16 +121,16 @@ public class TaskView extends ViewPart {
 			return new String[0];
 		}
 	}
-	
+
 	private class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
 		public String getColumnText(Object obj, int index) {
 			if (obj instanceof TaskSummary) {
 				TaskSummary taskSummary = (TaskSummary) obj;
 				switch (index) {
-					case 0: 
+					case 0:
 						return taskSummary.getName();
 					case 1:
-						Status status = taskSummary.getStatus(); 
+						Status status = taskSummary.getStatus();
 						return status == null ? null : STATUSSES.get(status);
 					case 2:
 						User user = taskSummary.getActualOwner();
@@ -156,7 +157,7 @@ public class TaskView extends ViewPart {
 			return null;
 		}
 	}
-	
+
 	public TaskView() {
 		IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
     	ipAddress = preferenceStore.getString(DroolsTaskConstants.SERVER_IP_ADDRESS);
@@ -174,7 +175,7 @@ public class TaskView extends ViewPart {
 			}
     	});
 	}
-	
+
 	public void createPartControl(Composite parent) {
 		parent.setLayout(new GridLayout(11, false));
 		Label userNameLabel = new Label(parent, SWT.NONE);
@@ -221,11 +222,11 @@ public class TaskView extends ViewPart {
 		GridData gridData = new GridData(GridData.FILL_BOTH);
 		gridData.grabExcessVerticalSpace = true;
 		gridData.horizontalSpan = 11;
-		table.setLayoutData(gridData);		
+		table.setLayoutData(gridData);
 		table.setLinesVisible(true);
 		table.setHeaderVisible(true);
 		// 1st column = task name
-		TableColumn column = new TableColumn(table, SWT.LEFT, 0);		
+		TableColumn column = new TableColumn(table, SWT.LEFT, 0);
 		column.setText("Name");
 		column.setWidth(300);
 		// 2nd column = task status
@@ -236,11 +237,11 @@ public class TaskView extends ViewPart {
 		column = new TableColumn(table, SWT.LEFT, 2);
 		column.setText("Owner");
 		column.setWidth(100);
-		// 4th column = expiration time 
+		// 4th column = expiration time
 		column = new TableColumn(table, SWT.CENTER, 3);
 		column.setText("Comment");
 		column.setWidth(120);
-		// 5th column = created 
+		// 5th column = created
 		column = new TableColumn(table, SWT.CENTER, 3);
 		column.setText("Created On");
 		column.setWidth(120);
@@ -251,7 +252,7 @@ public class TaskView extends ViewPart {
 		tableViewer.setUseHashlookup(true);
 		tableViewer.setColumnProperties(columnNames);
 	}
-	
+
 	private void createButtons(Composite parent) {
 		claimButton = new Button(parent, SWT.PUSH | SWT.CENTER);
 		claimButton.setText("Claim");
@@ -338,31 +339,31 @@ public class TaskView extends ViewPart {
 			}
 		});
 	}
-	
+
 	private void updateButtons() {
 		boolean selected = table.getSelectionCount() == 1;
 		TaskSummary task = getSelectedTask();
 		String userId = getUserId();
 		claimButton.setEnabled(selected && Status.Created.equals(task.getStatus()));
-		startButton.setEnabled(selected && 
+		startButton.setEnabled(selected &&
 			(Status.Ready.equals(task.getStatus()) ||
 				(Status.Reserved.equals(task.getStatus())
 					&& userId.equals(task.getActualOwner().getId()))));
 		stopButton.setEnabled(selected && Status.InProgress.equals(task.getStatus())
 			&& userId.equals(task.getActualOwner().getId()));
-		releaseButton.setEnabled(selected && 
+		releaseButton.setEnabled(selected &&
 			(Status.Reserved.equals(task.getStatus()) || Status.InProgress.equals(task.getStatus()))
 				&& userId.equals(task.getActualOwner().getId()));
-		suspendButton.setEnabled(selected && 
-			(Status.Ready.equals(task.getStatus()) || 
+		suspendButton.setEnabled(selected &&
+			(Status.Ready.equals(task.getStatus()) ||
 				((Status.Reserved.equals(task.getStatus()) || Status.InProgress.equals(task.getStatus()))
 					&& userId.equals(task.getActualOwner().getId()))));
 		// TODO only actual owner if previousStatus = reserved or inProgress
 		resumeButton.setEnabled(selected && Status.Suspended.equals(task.getStatus()));
 		// TODO only initiator if state Created
 		skipButton.setEnabled(selected && task.isSkipable() &&
-			(Status.Created.equals(task.getStatus()) && Status.Ready.equals(task.getStatus()) || 
-				((Status.Reserved.equals(task.getStatus()) || Status.InProgress.equals(task.getStatus())) 
+			(Status.Created.equals(task.getStatus()) && Status.Ready.equals(task.getStatus()) ||
+				((Status.Reserved.equals(task.getStatus()) || Status.InProgress.equals(task.getStatus()))
 					&& userId.equals(task.getActualOwner().getId()))));
 		completeButton.setEnabled(selected && Status.InProgress.equals(task.getStatus())
 			&& userId.equals(task.getActualOwner().getId()));
@@ -397,7 +398,7 @@ public class TaskView extends ViewPart {
 		manager.add(refreshAction);
 		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 	}
-	
+
 	private void fillLocalToolBar(IToolBarManager manager) {
 		manager.add(refreshAction);
 	}
@@ -422,18 +423,18 @@ public class TaskView extends ViewPart {
 	public void setFocus() {
 		tableViewer.getControl().setFocus();
 	}
-	
+
 	private void refresh() {
 		String userId = getUserId();
 		if (userId == null) {
 			return;
 		}
-		
+
 		MinaTaskClient client = getTaskClient();
 		if (client == null) {
 			return;
 		}
-		
+
 		try {
 			BlockingTaskSummaryResponseHandler responseHandler = new BlockingTaskSummaryResponseHandler();
 			client.getTasksAssignedAsPotentialOwner(userId, language, responseHandler);
@@ -451,7 +452,7 @@ public class TaskView extends ViewPart {
 	        tableViewer.setSelection(null);
 		}
 	}
-	
+
 	private void createTask() {
 		NewTaskDialog dialog = new NewTaskDialog(getSite().getShell());
 		int result = dialog.open();
@@ -466,209 +467,209 @@ public class TaskView extends ViewPart {
 			refresh();
 		}
 	}
-	
+
 	public void claim() {
 		MinaTaskClient client = getTaskClient();
 		if (client == null) {
 			return;
 		}
-		
+
 		String userId = getUserId();
 		if (userId == null) {
 			return;
 		}
-		
+
 		TaskSummary taskSummary = getSelectedTask();
 		if (taskSummary == null) {
 			return;
 		}
-		
+
 		BlockingTaskOperationResponseHandler responseHandler = new BlockingTaskOperationResponseHandler();
 		client.claim(taskSummary.getId(), userId, responseHandler);
 		responseHandler.waitTillDone(3000);
         refresh();
 	}
-	
+
 	public void start() {
 		MinaTaskClient client = getTaskClient();
 		if (client == null) {
 			return;
 		}
-		
+
 		String userId = getUserId();
 		if (userId == null) {
 			return;
 		}
-		
+
 		TaskSummary taskSummary = getSelectedTask();
 		if (taskSummary == null) {
 			return;
 		}
-		
+
 		BlockingTaskOperationResponseHandler responseHandler = new BlockingTaskOperationResponseHandler();
 		client.start(taskSummary.getId(), userId, responseHandler);
 		responseHandler.waitTillDone(3000);
         refresh();
 	}
-	
+
 	public void stop() {
 		MinaTaskClient client = getTaskClient();
 		if (client == null) {
 			return;
 		}
-		
+
 		String userId = getUserId();
 		if (userId == null) {
 			return;
 		}
-		
+
 		TaskSummary taskSummary = getSelectedTask();
 		if (taskSummary == null) {
 			return;
 		}
-		
+
 		BlockingTaskOperationResponseHandler responseHandler = new BlockingTaskOperationResponseHandler();
 		client.stop(taskSummary.getId(), userId, responseHandler);
 		responseHandler.waitTillDone(3000);
         refresh();
 	}
-	
+
 	public void release() {
 		MinaTaskClient client = getTaskClient();
 		if (client == null) {
 			return;
 		}
-		
+
 		String userId = getUserId();
 		if (userId == null) {
 			return;
 		}
-		
+
 		TaskSummary taskSummary = getSelectedTask();
 		if (taskSummary == null) {
 			return;
 		}
-		
+
 		BlockingTaskOperationResponseHandler responseHandler = new BlockingTaskOperationResponseHandler();
 		client.release(taskSummary.getId(), userId, responseHandler);
 		responseHandler.waitTillDone(3000);
         refresh();
 	}
-	
+
 	public void suspend() {
 		MinaTaskClient client = getTaskClient();
 		if (client == null) {
 			return;
 		}
-		
+
 		String userId = getUserId();
 		if (userId == null) {
 			return;
 		}
-		
+
 		TaskSummary taskSummary = getSelectedTask();
 		if (taskSummary == null) {
 			return;
 		}
-		
+
 		BlockingTaskOperationResponseHandler responseHandler = new BlockingTaskOperationResponseHandler();
 		client.suspend(taskSummary.getId(), userId, responseHandler);
 		responseHandler.waitTillDone(3000);
         refresh();
 	}
-	
+
 	public void resume() {
 		MinaTaskClient client = getTaskClient();
 		if (client == null) {
 			return;
 		}
-		
+
 		String userId = getUserId();
 		if (userId == null) {
 			return;
 		}
-		
+
 		TaskSummary taskSummary = getSelectedTask();
 		if (taskSummary == null) {
 			return;
 		}
-		
+
 		BlockingTaskOperationResponseHandler responseHandler = new BlockingTaskOperationResponseHandler();
 		client.resume(taskSummary.getId(), userId, responseHandler);
 		responseHandler.waitTillDone(3000);
         refresh();
 	}
-	
+
 	public void skip() {
 		MinaTaskClient client = getTaskClient();
 		if (client == null) {
 			return;
 		}
-		
+
 		String userId = getUserId();
 		if (userId == null) {
 			return;
 		}
-		
+
 		TaskSummary taskSummary = getSelectedTask();
 		if (taskSummary == null) {
 			return;
 		}
-		
+
 		BlockingTaskOperationResponseHandler responseHandler = new BlockingTaskOperationResponseHandler();
 		client.skip(taskSummary.getId(), userId, responseHandler);
 		responseHandler.waitTillDone(3000);
         refresh();
 	}
-	
+
 	public void complete() {
 		MinaTaskClient client = getTaskClient();
 		if (client == null) {
 			return;
 		}
-		
+
 		String userId = getUserId();
 		if (userId == null) {
 			return;
 		}
-		
+
 		TaskSummary taskSummary = getSelectedTask();
 		if (taskSummary == null) {
 			return;
 		}
-		
+
 		BlockingTaskOperationResponseHandler responseHandler = new BlockingTaskOperationResponseHandler();
 		client.complete(taskSummary.getId(), userId, null, responseHandler);
 		responseHandler.waitTillDone(3000);
         refresh();
 	}
-	
+
 	public void fail() {
 		MinaTaskClient client = getTaskClient();
 		if (client == null) {
 			return;
 		}
-		
+
 		String userId = getUserId();
 		if (userId == null) {
 			return;
 		}
-		
+
 		TaskSummary taskSummary = getSelectedTask();
 		if (taskSummary == null) {
 			return;
 		}
-		
+
 		BlockingTaskOperationResponseHandler responseHandler = new BlockingTaskOperationResponseHandler();
 		client.fail(taskSummary.getId(), userId, null, responseHandler);
 		responseHandler.waitTillDone(3000);
         refresh();
 	}
-	
+
 	private MinaTaskClient getTaskClient() {
 		if (client == null) {
 			client = new MinaTaskClient(
-				"org.drools.eclipse.task.views.TaskView", new TaskClientHandler());
+				"org.drools.eclipse.task.views.TaskView", new TaskClientHandler(SystemEventListenerFactory.getSystemEventListener()));
 			NioSocketConnector connector = new NioSocketConnector();
 			SocketAddress address = new InetSocketAddress(ipAddress, port);
 			boolean connected = client.connect(connector, address);
@@ -679,18 +680,18 @@ public class TaskView extends ViewPart {
 		}
 		return client;
 	}
-	
+
 	public void dispose() {
 		if (client != null) {
 			client.disconnect();
 		}
 		super.dispose();
 	}
-	
+
 	private String getUserId() {
 		return userNameText.getText();
 	}
-	
+
 	private TaskSummary getSelectedTask() {
 		ISelection selection = tableViewer.getSelection();
 		if (selection instanceof StructuredSelection) {
@@ -701,5 +702,5 @@ public class TaskView extends ViewPart {
 		}
 		return null;
 	}
-	
+
 }
