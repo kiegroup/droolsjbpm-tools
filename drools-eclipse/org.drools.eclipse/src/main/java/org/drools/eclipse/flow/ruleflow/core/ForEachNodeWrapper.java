21aff26c31c28f68f67e0ea8f1f9558b8698a457
Kris Verlaenen <kris.verlaenen@gmail.com>
2010-02-20 15:47:03 +0000

    - made default type of variable in ForEach node java.lang.Object
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@31754 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-eclipse/org.drools.eclipse/src/main/java/org/drools/eclipse/flow/ruleflow/core/ForEachNodeWrapper.java
+++ drools-eclipse/org.drools.eclipse/src/main/java/org/drools/eclipse/flow/ruleflow/core/ForEachNodeWrapper.java
@@ -93,7 +93,7 @@ public class ForEachNodeWrapper extends CompositeNodeWrapper {
 
     public void setPropertyValue(Object id, Object value) {
         if (VARIABLE_NAME.equals(id)) {
-            getForEachNode().setVariable((String) value, new ObjectDataType()); 
+            getForEachNode().setVariable((String) value, new ObjectDataType("java.lang.Object")); 
         } else if (COLLECTION_EXPRESSION.equals(id)) {
             getForEachNode().setCollectionExpression((String) value);
         } else if (START_NODE.equals(id)) {
