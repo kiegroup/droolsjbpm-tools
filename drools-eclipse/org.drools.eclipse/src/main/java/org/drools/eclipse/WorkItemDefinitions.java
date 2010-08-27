158fb5718cf2dde477cfb830ac93115e2971eb50
Kris Verlaenen <kris.verlaenen@gmail.com>
2010-08-27 14:47:52 +0000

    JBRULES-2679: Modularize Drools Flow  - extracted drools-flow-core from drools-core
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@34923 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-eclipse/org.drools.eclipse/src/main/java/org/drools/eclipse/WorkItemDefinitions.java
+++ drools-eclipse/org.drools.eclipse/src/main/java/org/drools/eclipse/WorkItemDefinitions.java
@@ -17,11 +17,18 @@
 package org.drools.eclipse;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.drools.RuleBaseConfiguration;
 import org.drools.eclipse.util.ProjectClassLoader;
+import org.drools.process.core.ParameterDefinition;
 import org.drools.process.core.WorkDefinition;
+import org.drools.process.core.datatype.DataType;
+import org.drools.process.core.impl.ParameterDefinitionImpl;
+import org.drools.process.core.impl.WorkDefinitionExtensionImpl;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
@@ -55,19 +62,45 @@ public final class WorkItemDefinitions {
             ClassLoader oldLoader = Thread.currentThread().getContextClassLoader();
             ClassLoader newLoader = ProjectClassLoader.getProjectClassLoader(project);
             try {
+            	Map<String, WorkDefinition> workDefinitions = new HashMap<String, WorkDefinition>();
                 Thread.currentThread().setContextClassLoader(newLoader);
-                return new RuleBaseConfiguration().getProcessWorkDefinitions();
+                List<Map<String, Object>> workDefinitionsMaps = new RuleBaseConfiguration().getWorkDefinitions();
+                for ( Map<String, Object> workDefinitionMap : workDefinitionsMaps ) {
+                    if ( workDefinitionMap != null ) {
+                        WorkDefinitionExtensionImpl workDefinition = new WorkDefinitionExtensionImpl();
+                        workDefinition.setName( (String) workDefinitionMap.get( "name" ) );
+                        workDefinition.setDisplayName( (String) workDefinitionMap.get( "displayName" ) );
+                        workDefinition.setIcon( (String) workDefinitionMap.get( "icon" ) );
+                        workDefinition.setCustomEditor( (String) workDefinitionMap.get( "customEditor" ) );
+                        Set<ParameterDefinition> parameters = new HashSet<ParameterDefinition>();
+                        Map<String, DataType> parameterMap = (Map<String, DataType>) workDefinitionMap.get( "parameters" );
+                        if ( parameterMap != null ) {
+                            for ( Map.Entry<String, DataType> entry : parameterMap.entrySet() ) {
+                                parameters.add( new ParameterDefinitionImpl( entry.getKey(),
+                                                                             entry.getValue() ) );
+                            }
+                        }
+                        workDefinition.setParameters( parameters );
+                        Set<ParameterDefinition> results = new HashSet<ParameterDefinition>();
+                        Map<String, DataType> resultMap = (Map<String, DataType>) workDefinitionMap.get( "results" );
+                        if ( resultMap != null ) {
+                            for ( Map.Entry<String, DataType> entry : resultMap.entrySet() ) {
+                                results.add( new ParameterDefinitionImpl( entry.getKey(),
+                                                                          entry.getValue() ) );
+                            }
+                        }
+                        workDefinition.setResults( results );
+                        workDefinitions.put( workDefinition.getName(),
+                                                  workDefinition );
+                    }
+                }
+
+                return workDefinitions;
             } finally {
                 Thread.currentThread().setContextClassLoader(oldLoader);
             }
         }
         return new HashMap<String, WorkDefinition>();
-    }
-    
-    public static void main(String[] args) {
-        for (WorkDefinition def: new RuleBaseConfiguration().getProcessWorkDefinitions().values()) {
-            System.out.println(def);
-        }
     }
     
 }
