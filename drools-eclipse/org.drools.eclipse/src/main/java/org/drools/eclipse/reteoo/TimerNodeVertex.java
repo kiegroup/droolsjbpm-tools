60c768cbad80bedcbd6e507613213db7904d8f05
krisv <kris.verlaenen@gmail.com>
2013-11-06 15:07:04 +0100

    BZ-1027118 - Rete tree build error with timer  - added missing node types

---
+++ drools-eclipse/org.drools.eclipse/src/main/java/org/drools/eclipse/reteoo/TimerNodeVertex.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2010 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.eclipse.reteoo;
+
+import org.drools.core.reteoo.TimerNode;
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.swt.graphics.Color;
+
+/**
+ * Wraps {@link org.drools.core.reteoo.TimerNode} and adds visual extras like color information
+ */
+public class TimerNodeVertex extends BaseVertex {
+    
+    private static final String NODE_NAME = "TimerNode";
+    
+    private final TimerNode node;
+
+    /**
+     * Constructor
+     * 
+     * @param node node to be wrapped
+     */
+    public TimerNodeVertex(final TimerNode node) {
+        super();
+        this.node = node;
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.core.reteoo.BaseNodeVertex#getHtml()
+     */
+    public String getHtml() {
+        return NODE_NAME+" : " + this.node.getId() + this.node.getTimer();
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.eclipse.editors.rete.model.BaseVertex#toString()
+     */
+    public String toString() {
+        return NODE_NAME;
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.core.reteoo.BaseNodeVertex#getFillColor()
+     */
+    public Color getFillColor() {
+        return ColorConstants.cyan;
+    }
+
+    /**
+     * Node ID
+     * 
+     * @return node id
+     */
+    public int getId() {
+        return node.getId();
+    }
+
+}
