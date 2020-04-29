5b9a15e13c6d8a8e3a5b42279a187b7bc75b00ea
mariofusco <mario.fusco@gmail.com>
2020-04-29 10:04:33 +0200

    [DROOLS-2965] add new vertex mocking new AlphaTerminalNode

---
+++ drools-eclipse/org.drools.eclipse/src/main/java/org/drools/eclipse/reteoo/AlphaTerminalNodeVertex.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2010 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.eclipse.reteoo;
+
+import org.drools.core.reteoo.AlphaTerminalNode;
+import org.drools.core.reteoo.LeftInputAdapterNode;
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.swt.graphics.Color;
+
+/**
+ * Wraps {@link LeftInputAdapterNode} and adds visual extras like color information
+ */
+public class AlphaTerminalNodeVertex extends BaseVertex {
+
+    private static final String        NODE_NAME = "AlphaTerminalNode";
+
+    private final AlphaTerminalNode node;
+
+    /**
+     * Constructor
+     *
+     * @param node node to be wrapped
+     */
+    public AlphaTerminalNodeVertex( final AlphaTerminalNode node) {
+        super();
+        this.node = node;
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.core.reteoo.BaseNodeVertex#getHtml()
+     */
+    public String getHtml() {
+        return NODE_NAME;
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.eclipse.editors.rete.model.BaseVertex#toString()
+     */
+    public String toString() {
+        return this.node.toString();
+    }
+
+    /* (non-Javadoc)
+     * @see org.drools.core.reteoo.BaseNodeVertex#getFillColor()
+     */
+    public Color getFillColor() {
+        return ColorConstants.yellow;
+    }
+
+}
